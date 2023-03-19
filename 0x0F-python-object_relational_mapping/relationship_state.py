858a70812708d1c81b23e80a4a7f2fa43c7e98f6
lolaibikz <ibikunle.oluwafunmilola@gmail.com>
2023-03-19 16:26:28 -0700

    all done

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_city import Base, City
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
