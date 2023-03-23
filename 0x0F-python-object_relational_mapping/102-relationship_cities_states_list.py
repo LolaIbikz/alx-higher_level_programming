744bf2be3252a3cd2f9fcb44c1f1b17b5445f51d
lolaibikz <ibikunle.oluwafunmilola@gmail.com>
2023-03-23 12:31:05 -0700

    relational mapping

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+lists all City objects from the database hbtn_0e_101_usa
+"""
 
 import sys
 from sqlalchemy import create_engine
@@ -7,12 +10,12 @@ from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
-
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    for city in session.query(City).order_by(City.id):
-         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    for city in session.query(City).order_by(City.id).all():
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    session.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""
+displays all values in the states table of
+ hbtn_0e_0_usa where name matches the argument
+"""
 
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s;", (sys.argv[4],))
-    [print(states) for states in cur.fetchall()]
+    c = db.cursor()
+    c.execute("""SELECT * FROM states""")
